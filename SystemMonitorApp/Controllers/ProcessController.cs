cc350af8ed9d5191fac6265c7cadd90849c78940
Anton Petrenko <petrenko.anton22@gmail.com>
2025-07-25 11:23:13 +0300

    Add process info enhancements and new API endpoint
    Updated `ProcessInfo` class with `UserName` and `IsSystemProcess` properties. Modified `Program.cs` to register `IProcessService` for dependency injection. Introduced `ProcessController` with a new endpoint to retrieve active processes.

---
+++ SystemMonitorApp/Controllers/ProcessController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SystemMonitor.Api.Models;
+using SystemMonitor.Api.Services;
+
+namespace SystemMonitor.Api.Controllers;
+
+[Route("api/[controller]")]
+public class ProcessController(IProcessService processService) : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<ProcessInfo> GetActiveProcesses()
+    {
+        return processService.GetActiveProcesses();
+    }
+}
--- SystemMonitorApp/Program.cs
+++ SystemMonitorApp/Program.cs
@@ -1,3 +1,5 @@
+using SystemMonitor.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
@@ -5,6 +7,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IProcessService, ProcessService>();
 
 var app = builder.Build();
 
